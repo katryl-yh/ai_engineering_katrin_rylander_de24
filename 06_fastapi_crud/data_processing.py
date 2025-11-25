caaa6098ea8059c99aef2ae476a46c47838b756d
Katrin katryl-yh <katrin@rylander.eu>
2025-11-25 14:32:17 +0100

    followed 06 lecture

--- 06_fastapi_crud/data_processing.py
+++ 06_fastapi_crud/data_processing.py
@@ -3,13 +3,14 @@ import pprint
 from constants import DATA_PATH, CURRENT_YEAR
 from pydantic import BaseModel, Field, field_validator
 
-
+# convenience function to read json data
 def read_json(filename):
     with open(DATA_PATH / filename, "r") as file:
         data = json.load(file)
 
     return data
 
+# pydantic models 
 class Book(BaseModel):
     id: int
     title: str
@@ -31,9 +32,21 @@ class Library(BaseModel):
     name: str
     books: list[Book]
 
+# data validation function
 def library_data(filename):
     json_data = read_json(filename)
     return Library.model_validate(json_data)
 
+class YearFilter(BaseModel):
+    start_year: int = Field(1800, gt=1000, lt=2026)
+    end_year: int = Field(2000, gt=1000, lt=2026)
+
+    @field_validator("end_year")
+    @classmethod
+    def validate_end_year(cls, value, info):
+        if value <= info.data.get("start_year"):
+            raise ValueError("end_year must be greater than start_year")
+        return value
+    
 if __name__ == "__main__":
     print(repr(read_json("library.json")))
