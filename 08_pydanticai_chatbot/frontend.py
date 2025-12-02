e9dc0cb6e4114d00f659f5d550f2d0e141e6e790
Katrin katryl-yh <katrin@rylander.eu>
2025-12-02 10:55:41 +0100

    add frontend

--- 08_pydanticai_chatbot/frontend.py
+++ 08_pydanticai_chatbot/frontend.py
@@ -29,4 +29,15 @@ def handle_user_input():
         with st.chat_message("assistant"):
             st.markdown(response)
         
-        st.session_state.messages.append("role":"assistant", "content": response)
+        st.session_state.messages.append({"role": "assistant", "content": response})
+
+def layout():
+    st.markdown("# Chat with RoBÅT")
+    st.markdown("RoBÅT is a funny robot that jokes about programming")
+
+    display_chat_messages()
+    handle_user_input()
+
+if __name__ == "__main__":
+    init_session_states()
+    layout()
